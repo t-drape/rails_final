3e96741f15f7748dd3ddfab442e398ac09dcc0a4
t-drape <thebatreview1@gmail.com>
2025-03-21 12:46:29 -0700

    Remove ormpt from devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -28,8 +28,7 @@ Devise.setup do |config|
   config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
   config.omniauth :google_oauth2,
    Rails.application.credentials.dig(:google_oauth_client_id),
-    Rails.application.credentials.dig(:google_oauth_client_secret),
-    prompt: "consent"
+    Rails.application.credentials.dig(:google_oauth_client_secret)
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
